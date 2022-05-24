623cfa77ddc8d67ef48e4b3e3451bf25f98efe88
bren-10 <info@bren-10.co.za>
2022-05-24 18:21:12 +0200

    Fix password hashing bug

--- pages/api/register.js
+++ pages/api/register.js
@@ -2,13 +2,25 @@ import Mongoose from "mongoose";
 import { ClientModel } from "../../mongoose/models";
 import bcrypt from 'bcrypt'
 
-export default async function handler(req, res) {
-  const clData = req.body
+
+async function hashPassword(pw) {
   const saltRounds = 10
 
-  bcrypt.hash(clData.password, saltRounds, function(err, hash) {
-    clData.password = hash
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(pw, saltRounds, function(err, hash) {
+      if (err) reject(err)
+      resolve(hash)
+    });
   })
+
+  return hashedPassword
+}
+
+export default async function handler(req, res) {
+  const clData = req.body
+
+  const hashedPw = await hashPassword(clData.password)
+  clData.password = hashedPw
 
   Mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
